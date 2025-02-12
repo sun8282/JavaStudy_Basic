8573a30fb341187f1d632d57280a67669367d3eb
SeungSoo Ha <109707901+sun8282@users.noreply.github.com>
2025-02-12 15:31:44 +0900

    add: 02-12 다형성 ex3

---
+++ java-basic/src/poly/car1/NewCar.java
@@ -0,0 +1,18 @@
+package poly.car1;
+
+public class NewCar implements Car{
+    @Override
+    public void startEngine() {
+        
+    }
+
+    @Override
+    public void offEngine() {
+
+    }
+
+    @Override
+    public void pressAccelerator() {
+
+    }
+}
